71b0e6155e9c139886aba2368c7a05e0cb4d10c1
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-07-10 14:41:31 -0400

    test

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#"> MyBrand </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="#home">Home</a>
+        <a class="navbar-item" href="#about">About</a>
+        <a class="navbar-item" href="#contact">Contact</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+}
+
+.navbar-item {
+  color: #fff;
+  text-decoration: none;
+  margin: 0 0.5rem;
+}
+
+.navbar-item:hover {
+  text-decoration: underline;
+}
+</style>
