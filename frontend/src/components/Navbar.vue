ac4041546d7c4c449a180ed943c5e7d271c96cc6
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-07-10 15:19:33 -0400

    Fixed HMR & created Navbar prototype
    Edited Dockerfile & docker-compose.yml in order to fix error where any updates would not show up.
    
    Also added a Navbar component. This will need to be refactored to be more reusable - hard coding all values within navbar is not ideal. We will want to reuse this component later when implementing the sub-navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -5,9 +5,24 @@
     </div>
     <div class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item" href="#home">Home</a>
-        <a class="navbar-item" href="#about">About</a>
-        <a class="navbar-item" href="#contact">Contact</a>
+        <a class="navbar-item" href="#bar"
+          ><i class="fas fa-cocktail"></i> Bar</a
+        >
+        <a class="navbar-item" href="#construction"
+          ><i class="fas fa-hard-hat"></i> Construction</a
+        >
+        <a class="navbar-item" href="#food"
+          ><i class="fas fa-utensils"></i> Food</a
+        >
+        <a class="navbar-item" href="#legal"
+          ><i class="fas fa-balance-scale"></i> Legal</a
+        >
+        <a class="navbar-item" href="#restaurant"
+          ><i class="fas fa-utensil-spoon"></i> Restaurant</a
+        >
+        <a class="navbar-item" href="#wine"
+          ><i class="fas fa-wine-bottle"></i> Wine</a
+        >
       </div>
     </div>
   </nav>
