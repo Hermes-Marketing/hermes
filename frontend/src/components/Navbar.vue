1f38c1fe8a6e90b0e147be49505399c4fffc7315
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-07-20 13:30:30 -0400

    SubNavbar Template
    Created SubNavbar Template. This includes all required sub-categories which are expanded when their category is selected.
    
    Still need to add event trackers in order to properly pass the categories and subcategories as an object to the cards.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,40 +1,88 @@
 <template>
-  <nav class="navbar">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="#"> MyBrand </a>
-    </div>
-    <div class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item" href="#home">Home</a>
-        <a class="navbar-item" href="#about">About</a>
-        <a class="navbar-item" href="#contact">Contact</a>
-        <a class="navbar-item" href="#bar"
-          ><i class="fas fa-cocktail"></i> Bar</a
-        >
-        <a class="navbar-item" href="#construction"
-          ><i class="fas fa-hard-hat"></i> Construction</a
-        >
-        <a class="navbar-item" href="#food"
-          ><i class="fas fa-utensils"></i> Food</a
-        >
-        <a class="navbar-item" href="#legal"
-          ><i class="fas fa-balance-scale"></i> Legal</a
-        >
-        <a class="navbar-item" href="#restaurant"
-          ><i class="fas fa-utensil-spoon"></i> Restaurant</a
-        >
-        <a class="navbar-item" href="#wine"
-          ><i class="fas fa-wine-bottle"></i> Wine</a
-        >
+  <div>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#"> MyBrand </a>
       </div>
-    </div>
-  </nav>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a
+            v-for="item in mainNavItems"
+            :key="item.name"
+            class="navbar-item"
+            href="#"
+            @click="selectItem(item.name)"
+          >
+            <span v-html="item.icon"></span> {{ item.name }}
+          </a>
+        </div>
+      </div>
+    </nav>
+    <SubNavbar :subItems="subNavItems" />
+  </div>
 </template>
+
 <script>
+import SubNavbar from "./SubNavbar.vue";
+
 export default {
   name: "Navbar",
+  components: {
+    SubNavbar,
+  },
+  data() {
+    return {
+      mainNavItems: [
+        { name: "Bar", icon: '<i class="fas fa-cocktail" />' },
+        { name: "Construction", icon: '<i class="fas fa-hard-hat" />' },
+        { name: "Food", icon: '<i class="fas fa-utensils" />' },
+        { name: "Legal", icon: '<i class="fas fa-balance-scale" />' },
+        { name: "Restaurant", icon: '<i class="fas fa-utensil-spoon" />' },
+        { name: "Wine", icon: '<i class="fas fa-wine-bottle" />' },
+      ],
+      subNavItems: [],
+    };
+  },
+  methods: {
+    selectItem(item) {
+      const subNavMapping = {
+        Bar: [
+          "Tap and Social",
+          "Lounge",
+          "Pub",
+          "Barcade",
+          "Tavern",
+          "Sports bar",
+          "Bar and Grill",
+          "Taproom",
+          "Brewery",
+        ],
+        Construction: [
+          "Roofing",
+          "Residential Homes",
+          "Services",
+          "Contracting",
+        ],
+        Food: ["Catering", "Butcher & Grocer"],
+        Legal: ["Attorney", "Estate", "Litigation"],
+        Restaurant: [
+          "Sandwiches",
+          "Steak house",
+          "Steak",
+          "Diner",
+          "Family",
+          "Fine Dining",
+          "Italian",
+          "Modern Cuisine",
+        ],
+        Wine: ["Winery", "Wine and Spirits", "Vineyards"],
+      };
+      this.subNavItems = subNavMapping[item] || [];
+    },
+  },
 };
 </script>
+
 <style scoped>
 .navbar {
   display: flex;
---
+++ frontend/src/components/SubNavbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="sub-navbar" v-if="subItems.length">
+    <div class="sub-navbar-menu">
+      <div class="sub-navbar-start">
+        <a
+          v-for="item in subItems"
+          :key="item"
+          class="sub-navbar-item"
+          href="#"
+        >
+          {{ item }}
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "SubNavbar",
+  props: {
+    subItems: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sub-navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem;
+  background-color: #444;
+  color: #fff;
+}
+.sub-navbar-item {
+  color: #fff;
+  text-decoration: none;
+  margin: 0 0.5rem;
+}
+.sub-navbar-item:hover {
+  text-decoration: underline;
+}
+</style>
