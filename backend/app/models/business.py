1623c12f4dd8e02a1163ff885b572c1a2ee43bdb
Connor Logan <connorlogan00@gmail.com>
2024-07-22 18:24:14 -0400

    6: pushing updated model

--- backend/app/models/business.py
+++ backend/app/models/business.py
@@ -3,8 +3,8 @@
     ~~~~~~~~~~~~~~~~~~~
     Defines the business model for the application
 """
-
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,20 +12,20 @@ Base = declarative_base()
 class Business(Base):
     __tablename__ = 'businesses'
     
-    id = Column(String, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     category = Column(String)
-    sub_category = Column(String)
-    description = Column(String)
+    sub_category = Column(String, nullable=True)  
+    description = Column(String, nullable=True)  
     first_name = Column(String)
     last_name = Column(String)
-    email_address = Column(String)
-    phone_number = Column(String)
+    email_address = Column(String, nullable=True)  
+    phone_number = Column(String, nullable=True)  
     brothers_role = Column(String)
     business_name = Column(String)
     business_location = Column(String)
-    website = Column(String)
-    chapter_affiliation = Column(String)
-    university_affiliation = Column(String)
+    website = Column(String, nullable=True)  
+    chapter_affiliation = Column(String, nullable=True)  
+    university_affiliation = Column(String, nullable=True)  
     street_address = Column(String)
     city = Column(String)
     state = Column(String)
