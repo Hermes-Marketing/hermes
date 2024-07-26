174171e51a287d30e8f3ca66f573792c63487b65
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-07-26 16:17:52 -0400

    Navbar Template creation
    Navbar template created including Vue Router, basic styling & icons (from font awesome).

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Title of stuff</h1>
+    <hr />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Navbar />
+  <router-view />
 </template>
 
 <style scoped>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">Hermes</router-link>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/admin" class="navbar-item">
+          <i class="fas fa-user-shield"></i> Admin
+        </router-link>
+        <a class="navbar-item" href="#bar">
+          <i class="fas fa-cocktail"></i> Bar
+        </a>
+        <a class="navbar-item" href="#construction">
+          <i class="fas fa-hard-hat"></i> Construction
+        </a>
+        <a class="navbar-item" href="#food">
+          <i class="fas fa-utensils"></i> Food
+        </a>
+        <a class="navbar-item" href="#legal">
+          <i class="fas fa-balance-scale"></i> Legal
+        </a>
+        <a class="navbar-item" href="#restaurant">
+          <i class="fas fa-utensil-spoon"></i> Restaurant
+        </a>
+        <a class="navbar-item" href="#wine">
+          <i class="fas fa-wine-bottle"></i> Wine
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+}
+.navbar-item {
+  color: #fff;
+  text-decoration: none;
+  margin: 0 0.5rem;
+}
+.navbar-item:hover {
+  text-decoration: underline;
+}
+</style>
