41517305fe3e613d891f32a5cb44bde070e47e70
Connor Logan <connorlogan00@gmail.com>
2024-07-26 18:59:22 -0400

    10: Implementing get by state endpoint (needs settings from ticket 34 before merging into main

--- backend/app/api/v1/endpoints/company.py
+++ backend/app/api/v1/endpoints/company.py
@@ -26,3 +26,16 @@ async def get_all_companies(db_session=Depends(get_db)):
     """
     
     return CompanyRepository(db_session).get_all_companies()
+@router.get("/{state}", status_code=status.HTTP_200_OK, response_model=List[Company])
+async def get_companies_by_state(state: str,db_session=Depends(get_db)):
+    """
+    Get all company records from the company collection by the given state
+
+    - Args: state (str) 
+        The state to filter the companies by
+
+    - Returns:
+            List[Company]: A list of all companies in the company collection filtered by the given state
+    """
+    
+    return CompanyRepository(db_session).get_by_state(state)
--- backend/app/core/company.py
+++ backend/app/core/company.py
@@ -54,3 +54,51 @@ class CompanyRepository(AppRepository):
             raise e
 
         return companies
+    def get_by_state(self, state: str) -> List[Company]:
+        """
+        Get all companies from the specified collection by the given state.
+
+        Args:
+            state (str): The state to filter by.
+
+        Returns:
+            List[Company]: A list of all companies in the company collection
+
+        Raises:
+            Exception: If no companies are found in the given state
+        """
+        companies = []
+        try:
+            docs = self.db.collection('companies').where('state', '==', state).stream()
+            if not docs:
+                raise Exception(f"No companies found in {state}")
+            for doc in docs:
+                company_data = doc.to_dict()
+                company = Company(
+                    firestore_id=doc.id,
+                    category=company_data.get('category'),
+                    sub_category=company_data.get('subcategory'),
+                    description=company_data.get('description'),
+                    first_name=company_data.get('first_name'),
+                    last_name=company_data.get('last_name'),
+                    email_address=company_data.get('email_address'),
+                    phone_number=company_data.get('phone_number'),
+                    brothers_role=company_data.get('brothers_role'),
+                    business_name=company_data.get('business_name'),
+                    business_location=company_data.get('business_location'),
+                    website=company_data.get('website'),
+                    chapter_affiliation=company_data.get('chapter_affiliation'),
+                    university_affiliation=company_data.get('university_affiliation'),
+                    street_address=company_data.get('street_address'),
+                    city=company_data.get('city'),
+                    state=company_data.get('state'),
+                    zip_code=company_data.get('zip_code'),
+                    country=company_data.get('country'),
+                    deleted_at=company_data.get('deleted_at')
+                )
+                companies.append(company)
+        except Exception as e:
+            logging.error("Error: %s", e)
+            raise e
+
+        return companies
