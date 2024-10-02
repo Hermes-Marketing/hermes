a56f85eabecc8b37a7c9eb765102c16f7c6a6619
Xavier Remacle <xavrema@gmail.com>
2024-10-02 12:02:12 -0400

    development test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,28 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: true,
-    port: process.env.HERMES_FE_PORT, // This is the port which we will use in docker
-    watch: {
-      usePolling: false,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [vue()],
+    server: {
+      host: isProduction ? '0.0.0.0' : 'localhost', // Use 'localhost' for local dev, and '0.0.0.0' for production if needed
+      port: process.env.HERMES_FE_PORT || (isProduction ? 443 : 8080), // Use 443 in prod and 8080 locally
+      watch: {
+        usePolling: false,
+      },
+      hmr: {
+        protocol: isProduction ? 'wss' : 'ws', // Use 'ws' locally and 'wss' in production
+        host: isProduction ? 'hermes.xavrema.com' : 'localhost',
+        port: isProduction ? 443 : (process.env.HERMES_FE_PORT || 8080), // Match the port with your server setup
+      }
     },
-    hmr:{
-      protocol: 'wss',
-      host: 'hermes.xavrema.com',
-      port: 443, // or the port your server is using for wss
-    }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
+  };
 });
