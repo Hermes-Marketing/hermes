be6fa1b141906aec358e96e5180972e19b311526
Xavier Remacle <xavrema@gmail.com>
2024-11-04 15:04:58 -0500

    setting base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import commonjs from "@rollup/plugin-commonjs"; // Add this line
 
 export default defineConfig({
+    base: '/',
     plugins: [vue(), commonjs()], // Include commonjs plugin
 });
